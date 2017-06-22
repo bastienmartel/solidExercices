1498c3319ac63dd3a5202d3bf6546e1d823c6a3a
MARTEL BASTIEN p1404247 <p1404247@univ-lyon1.fr>
2017-06-22 15:35:02 +0200

    4 opérations de base et tests simplistes

--- SolidExercices.Tests/CalculatorShould.cs
+++ SolidExercices.Tests/CalculatorShould.cs
@@ -12,5 +12,29 @@ namespace SolidExercices.Tests
             var result = calculator.Calculate("1+2,3");
             Check.That(result).IsEqualTo(3.3);
         }
+
+        [Test]
+        public void CalculateASubstraction()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Calculate("23-14,8");
+            Check.That(result).IsEqualTo(8.2);
+        }
+
+        [Test]
+        public void CalculateAProduct()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Calculate("5x7,5");
+            Check.That(result).IsEqualTo(37.5);
+        }
+
+        [Test]
+        public void CalculateADivision()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Calculate("45/22,5");
+            Check.That(result).IsEqualTo(2);
+        }
     }
 }
--- SolidExercices/Calculator.cs
+++ SolidExercices/Calculator.cs
@@ -7,7 +7,38 @@ namespace SolidExercices
     {
         public double Calculate(string operation)
         {
-            throw new NotImplementedException();
+            if (operation.Contains('+'))
+            {
+                string[] splitOperation = operation.Split('+');
+                return Convert.ToDouble(splitOperation[0]) + Convert.ToDouble(splitOperation[1]);
+            }
+            else if (operation.Contains('-'))
+            {
+                string[] splitOperation = operation.Split('-');
+                return Convert.ToDouble(splitOperation[0]) - Convert.ToDouble(splitOperation[1]);
+            }
+            else if (operation.Contains('x'))
+            {
+                string[] splitOperation = operation.Split('x');
+                return Convert.ToDouble(splitOperation[0]) * Convert.ToDouble(splitOperation[1]);
+            }
+            else if (operation.Contains('/'))
+            {
+                string[] splitOperation = operation.Split('/');
+                /*
+                if (Convert.ToDouble(splitOperation[0]) == 0 || Convert.ToDouble(splitOperation[1]) == 0)
+                {
+                    throw ImpossibleOperation("Impossible to divide a number by 0");
+                }
+                */
+                return Convert.ToDouble(splitOperation[0]) / Convert.ToDouble(splitOperation[1]);
+            }
+            else
+            {
+                return 0;
+            }
+            //throw new NotImplementedException();
         }
+
     }
 }
